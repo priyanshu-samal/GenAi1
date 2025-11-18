39bab0dd49915ba7e63ba2def13da2dd08a43195
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-11-18 12:46:36 +0530

    ok

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const groq=new Groq({apiKey: process.env.GROQ_API_KEY})
 
 async function main(){
            const completion=await groq.chat.completions.create({
-
+            temperature:1,
+            
             model:'llama-3.3-70b-versatile',
             messages:[
                 {
