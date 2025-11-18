d48c3987bc2da210ceaf04ec6c31ce4a75e6e6f1
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-11-18 13:55:04 +0530

    k

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-configDotenv()
+configDotenv();
 import { configDotenv } from "dotenv";
 import Groq from "groq-sdk";
 
-const groq=new Groq({apiKey: process.env.GROQ_API_KEY})
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-async function main(){
-           const completion=await groq.chat.completions.create({
-            temperature:1,
+async function main() {
+  const completion = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are Jarvis smart asistence that give answer like tony starks jarves short and only answer you have access to 1.webSearch({query}): {query: string}) // search latest on linternet",
+      },
+      {
+        role: "user",
+        content: "when i was iphone 16 launched?",
+      },
+    ],
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description: "latest search latest info and news about india ",
+          parameters: {
+            type: "object",
+            properties: {
+              location: {
+                type: "string",
+                description: "The city and state, e.g. San Francisco, CA",
+              },
+              
+            },
+            required:['query']
             
-            model:'llama-3.3-70b-versatile',
-            messages:[
-                {
-                    role:'system',
-                    content:'You are Jarvis smart asistence that give answer like tony starks jarves'
-                },
-                {
-                    role:'user',
-                    content:'Hi'
-                }
-            ]
-           })
-   console.log(completion.choices[0])
-           
+          },
+        },
+      },
+    ],
+    tool_choice: "auto",
+  });
+  console.log(completion.choices[0].message, null, 2);
 }
 
-main()
+main();
+
+async function webSearch({ query }) {
+  return console.log("hey")
+}
